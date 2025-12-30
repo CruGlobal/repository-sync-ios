a6cdcc632d6090bc331deab01ea4ce53acae9ba6
Levi Eggert <levi.eggert@cru.org>
2025-12-30 12:48:59 -0500

    Remove open methods

--- Sources/RepositorySync/RepositorySync.swift
+++ Sources/RepositorySync/RepositorySync.swift
@@ -21,17 +21,6 @@ open class RepositorySync<DataModelType: Sendable, ExternalDataFetchType: Extern
         self.externalDataFetch = externalDataFetch
         self.persistence = persistence
     }
-    
-    // MARK: - Persistence
-    
-    open func getRealmPersistence<T: IdentifiableRealmObject>() -> RealmRepositorySyncPersistence<DataModelType, ExternalDataFetchType.ExternalObject, T>? {
-        return persistence as? RealmRepositorySyncPersistence<DataModelType, ExternalDataFetchType.ExternalObject, T>
-    }
-    
-    @available(iOS 17.4, *)
-    open func getSwiftPersistence<T: IdentifiableSwiftDataObject>() -> SwiftRepositorySyncPersistence<DataModelType, ExternalDataFetchType.ExternalObject, T>? {
-        return persistence as? SwiftRepositorySyncPersistence<DataModelType, ExternalDataFetchType.ExternalObject, T>
-    }
 }
 
 // MARK: - External Data Fetch
